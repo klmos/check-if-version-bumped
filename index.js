8e03e69a55dc6f46c68c6e434e9282ad9e31a3a9
Almos Kovacs <almos.kovacs@warnermedia.com>
2024-05-27 16:03:11 +0200

    add enable init logic

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ try {
 
   const token = core.getInput('token');
   const path = core.getInput('path');
+  const enableInit = core.getBooleanInput('enable_init');
   const packageJsonPath = path ? path : 'package.json';
   const headers = {};
   if (token) {
@@ -25,14 +26,15 @@ try {
   const baseUrl = `https://raw.githubusercontent.com/${ github.context.repo.owner }/${ github.context.repo.repo }/${ baseSha }/${ packageJsonPath }`
 
   fetch(baseUrl, { headers })
-    .then(res => core.info(JSON.stringify(res)))
-    .then(res => res.json())
-    .then(res => res.version)
-    .then(version => {
-      const localVersion = require(`${ process.env.GITHUB_WORKSPACE }/${ packageJsonPath }`).version;
+    .then(res => {
+      const result = res.json();
+      if(enableInit && result.size == 0) core.info('New package detected. Pass on check.')
+      else {
+        const localVersion = require(`${ process.env.GITHUB_WORKSPACE }/${ packageJsonPath }`).version;
 
-      if (!semver.valid(localVersion)) core.setFailed(`Current version '${ localVersion }' detected as invalid one`);
-      if (!semver.gt(localVersion, version)) core.setFailed(`Version '${ localVersion }' wasn't detected as greater than '${ version }'`);
+        if (!semver.valid(localVersion)) core.setFailed(`Current version '${ localVersion }' detected as invalid one`);
+        if (!semver.gt(localVersion, result.version)) core.setFailed(`Version '${ localVersion }' wasn't detected as greater than '${ result.version }'`);
+      }
     })
     .catch(core.setFailed);
 } catch (error) {
